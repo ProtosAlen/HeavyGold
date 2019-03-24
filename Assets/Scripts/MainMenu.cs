fa5c97e322fffbf2a6661db71a2abd45a8a6c76c
ProtosAlen <alenn@intercellstudios.ga>
2019-03-24 17:40:47 +0100

    Fix

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject tutorial;
+    public GameObject settings;
+    public GameObject credits;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+     
+    public void ToggleTutorial()
+    {
+        if(tutorial.activeSelf)
+        {
+            tutorial.SetActive(false);
+        }
+        else
+        {
+            tutorial.SetActive(true);
+        }
+    }
+
+    public void ToggleCredits()
+    {
+        if (credits.activeSelf)
+        {
+            credits.SetActive(false);
+        }
+        else
+        {
+            credits.SetActive(true);
+        }
+    }
+
+    public void ToggleSettings()
+    {
+        if (settings.activeSelf)
+        {
+            settings.SetActive(false);
+        }
+        else
+        {
+            settings.SetActive(true);
+        }
+    }
+}
